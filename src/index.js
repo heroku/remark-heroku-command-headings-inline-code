9e7eca3a53bc66e3b5d83ee4bdbc54d3652fe0bf
Matthew Origer <moriger@heroku.com>
2017-08-22 11:57:59 -0500

    add npm test command

--- src/index.js
+++ src/index.js
@@ -3,18 +3,16 @@ const visit = require('unist-util-visit')
 const Is = require('unist-util-is')
 
 async function commandHeadingsHaveInlineCode (ast, file) {
-
   const validate = async (node) => {
     if (node.depth !== 3) return
     let inlineCodeFound = false
     for (let child of node.children) {
       if (Is('inlineCode', child)) inlineCodeFound = true
     }
-    if (inlineCodeFound === false)
-      file.message('Command heading without inline code', node)
+    if (inlineCodeFound === false) { file.message('Command heading without inline code', node) }
   }
 
   await visit(ast, 'heading', validate)
 }
 
-module.exports = rule('remark-lint:no-dead-urls', commandHeadingsHaveInlineCode)
+module.exports = rule('remark-lint:command-headings-inline-code', commandHeadingsHaveInlineCode)
